b0e933bda4f21ccff62851bbd9f6776446aa9bbb
ferdswa <maximcarr03@hotmail.com>
2026-02-18 14:02:37 +0000

    -d: delete old files

--- src/main/java/com/snake/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * A program based on snakee-v7-src that has been modified heavily and extended to include more levels, different sprites, a high score system, user settings and has been changed from Swing to JavaFX
- * @author Maxim Carr
- */
-package com.snake;
